814448bd746058c6cc3f1dabffec48902fcb5a83
Nazar Lesiv <nazarlesiv@MacBookPro-14-M1.fios-router.home>
2024-01-11 17:05:44 -0500

    Update standardization and tensorflow refactor

--- regressions/index.js
+++ regressions/index.js
@@ -20,5 +20,8 @@ const regression = new LinearRegression(features, labels, {
 
 regression.train();
 
-console.log("Updated M is: ", regression.m);
-console.log("updated B is: ", regression.b);
+// console.log("Updated M is: ", regression.weights.get(1, 0));
+// console.log("updated B is: ", regression.weights.get(0, 0));
+const r2 = regression.test(testFeatures, testLabels);
+
+console.log("R2 is: ", r2);
--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -2,19 +2,26 @@ const tf = require("@tensorflow/tfjs");
 const _ = require("lodash");
 class LinearRegression {
     constructor(features, labels, options) {
-        this.features = features;
-        this.labels = labels;
+        this.features = this.processFeatures(features);
+        this.labels = tf.tensor(labels);
 
         this.options = Object.assign({
             learningRate: 0.1,
             iterations: 1000
         }, options);
 
-        this.m = 0;
-        this.b = 0;
+        this.weights = tf.zeros([2, 1]);
     }
 
     gradientDescent() {
+        const currentGuesses = this.features.matMul(this.weights);
+        const differences = currentGuesses.sub(this.labels);
+
+        const slopes = this.features.transpose().matMul(differences).div(this.features.shape[0]);
+
+        this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    }
+    gradientDescentOld() {
         const currentGuessesForMPG = this.features.map(row => {
             return this.m * row[0] + this.b;
         })
@@ -36,6 +43,32 @@ class LinearRegression {
         for (let i = 0; i < this.options.iterations; ++i) {
             this.gradientDescent();
         }
+    }
+
+    test(testFeatures, testLabels) {
+        testFeatures = this.processFeatures(testFeatures);
+        testLabels = tf.tensor(testLabels);
+        const predictions = testFeatures.matMul(this.weights);
+
+        // predictions.print()
+
+        const res = testLabels.sub(predictions).pow(2).sum().get();
+        const tot = testLabels.sub(testLabels.mean()).pow(2).sum().get();
+
+        return 1 - res/tot;
+    }
+
+    processFeatures(features) {
+        features = tf.tensor(features)
+        features =tf.ones([features.shape[0], 1]).concat(features, 1); 
+        return features;
+    }
+
+    standardize(features) {
+        const { mean , variance } = tf.moments(features, 0);
+
+        this.mean = mean;
+        this.variance = variance;
     }
 }
 
