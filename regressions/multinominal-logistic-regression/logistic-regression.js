130170b6e25032b1a06def98f38b275e41568d4e
Nazar Lesiv <nazarlesiv@MacBookAir-15-M2.fios-router.home>
2024-01-20 09:39:44 -0500

    Initial image recognition example

--- regressions/multinominal-logistic-regression/logistic-regression.js
+++ regressions/multinominal-logistic-regression/logistic-regression.js
@@ -82,11 +82,12 @@ class LogisticRegression {
 
   standardize(features) {
     const { mean, variance } = tf.moments(features, 0);
-
+    // fix the division by 0 in the variance
+    const filler = variance.cast('bool').logicalNot().cast("float32");
     this.mean = mean;
-    this.variance = variance;
+    this.variance = variance.add(filler);
 
-    return features.sub(mean).div(variance.pow(0.5));
+    return features.sub(mean).div(this.variance.pow(0.5));
   }
 
   recordCost() {
